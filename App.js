02b6f2af0e3f59dec2314e0a83463994acd53326
Saumya Bhatnagar <saumya.bhatnagar@credgenics.com>
2022-12-23 06:35:11 +0530

    ADded screens for verification flows

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import type {Node} from 'react';
 import {
   SafeAreaView,
@@ -25,6 +25,11 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import {LoginScreen} from './src/screens/Login';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Portfolio} from './src/screens/Portfolio';
+import {PortfolioDetail} from './src/screens/PortfolioDetail';
 
 /* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
  * LTI update could not be added via codemod */
@@ -54,44 +59,24 @@ const Section = ({children, title}): Node => {
   );
 };
 
-const App: () => Node = () => {
+const Stack = createNativeStackNavigator();
+
+const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      {/* <SafeAreaView style={backgroundStyle}> */}
+      <Stack.Navigator
+        // screenOptions={{headerShown: false}}
+        // initialRouteName="Portfolio"
+        initialRouteName={'Portfolio'}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Portfolio" component={Portfolio} />
+        <Stack.Screen name="PortfolioDetail" component={PortfolioDetail} />
+      </Stack.Navigator>
+      {/* </SafeAreaView> */}
+    </NavigationContainer>
   );
 };
 
